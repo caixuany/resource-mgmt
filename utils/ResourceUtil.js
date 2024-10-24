9bbc3eb379dc2a72386bf5c59b01c8fa3f431595
caixuan_y <ycaixuann@gmail.com>
2024-10-24 15:21:37 +0800

    Implemented the frontend for view-resources

--- utils/ResourceUtil.js
+++ utils/ResourceUtil.js
@@ -31,6 +31,16 @@ async function writeJSON(object, filename) {
                 return res.status(500).json({ message: error.message });
         }
     }
+
+    async function viewResources(req, res) {
+        try {
+            const allResources = await readJSON('utils/resources.json');
+            return res.status(201).json(allResources);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
+    
 module.exports = {
-    readJSON, writeJSON, addResource
+    readJSON, writeJSON, addResource, viewResources
 };
