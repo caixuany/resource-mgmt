c253da069eb04d6e4ec61fae3e146b70817efe81
caixuany <ycaixuann@gmail.com>
2024-11-20 10:42:08 +0800

    Created and tested the test cases for frontend testing

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 var express = require('express');
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var app = express();
 
-const PORT = process.env.PORT || 5050
-var startPage = "index.html";
+const PORT = process.env.PORT || 5050;
+var startPage = 'index.html';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
-const { addResource, viewResources, editResource, deleteResource } = require('./utils/ResourceUtil')
+const {
+  addResource,
+  viewResources,
+  editResource,
+  deleteResource,
+} = require('./utils/ResourceUtil');
 app.post('/add-resource', addResource);
-app.get ('/view-resources', viewResources);
+app.get('/view-resources', viewResources);
 app.put('/edit-resource/:id', editResource);
 app.delete('/delete-resource/:id', deleteResource);
 
 app.get('/', (req, res) => {
-res.sendFile(__dirname + "/public/" + startPage);
-})
-
-server = app.listen(PORT, function () {
-    const address = server.address();
-    const baseUrl = `http://${address.address == "::" ? 'localhost' :
-address.address}:${address.port}`;
-    console.log(`Demo project at: ${baseUrl}`);
+  res.sendFile(__dirname + '/public/' + startPage);
 });
 
-module.exports = {app, server}
+server = app.listen(PORT, function () {
+  const address = server.address();
+  const baseUrl = `http://${
+    address.address == '::' ? 'localhost' : address.address
+  }:${address.port}`;
+  console.log(`Demo project at: ${baseUrl}`);
+});
+
+module.exports = { app, server };
